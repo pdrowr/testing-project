2c4efdde320d4f1375e702bd086f79efa0e583bf
pdrowr <pdrowr@gmail.com>
2019-05-03 08:39:34 -0400

    create products from the seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+products = [
+  ['Iphone X', 'Apple', 1000],
+  ['Iphone Xs', 'Apple', 900],
+  ['Huawei P30 Pro', 'Huawei', 800],
+  ['Huawei P30', 'Huawei', 650],
+  ['Huawei P20', 'Huawei', 300],
+  ['Huawei P20 Lite', 'Huawei', 200]
+]
+
+products.each do |product|
+  Product.create(
+    name:     product[0],
+    category: product[1],
+    price:    product[2]
+  )
+end
+
+puts "#{products.count} products has been created"
